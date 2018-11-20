0c3be9714b25ef33f58c1c302415b8286fae1d8d
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-20 12:22:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
     
   elsif phrase == "I LOVE YOU GRANDMA!"
-      "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
