6f78a6fa1740b2568510bd5471541922903ad104
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-20 12:20:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.uppercase
+  if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
     
   elsif phrase == "I LOVE YOU GRANDMA!"
-    
+      "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
