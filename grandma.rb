5277b81ebc27d3c6b801fe90e6f5d3793738928b
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-20 12:15:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,13 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma()
+def speak_to_grandma(phrase)
+  if phrase == phrase.uppercase
+    ------
+    
+  else
+    
+  end
+  
 end
 
 # Whatever you say to grandma, she should respond with
