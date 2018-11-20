3661d559afd4f5d8c2467f48223c000ac451c6b6
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-20 12:18:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.uppercase
-    ------
-    
+    return "NO, NOT SINCE 1938!"
   else
-    
     return "HUH?! SPEAK UP, SONNY!"
-    
   end
   
 end
